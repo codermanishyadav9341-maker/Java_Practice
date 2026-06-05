b16bbca053dc08fc7c8126eb8976be9f5e9794d6
codermanishyadav9341@gmail.com <codermanishyadav9341@gmail.com>
2026-06-05 20:23:54 +0530

    Update Code

--- src/Loops/Number.java
+++ src/Loops/Number.java
@@ -1,4 +1,15 @@
 package Loops;
 
+import java.util.Scanner;
 public class Number {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter number:- ");
+        int num = scan.nextInt();
+
+        for(int i = 0; i<=num; i++){
+            System.out.print(i);
+        }
+    }
 }
