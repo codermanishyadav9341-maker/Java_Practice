4d4d4709c924dcd52a43cd9f9adccb4025b3f592
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-15 16:37:34 +0530

    Initial commit

---
+++ src/Array/Length.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Scanner;
+public class Length {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Size:- ");
+        int size = scan.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter your Array elements:- ");
+        for(int i = 0; i<size; i++){
+            arr[i] = scan.nextInt();
+        }
+
+        System.out.print("Array length:- ");
+        for(int i = 0; i<size; i++){
+            System.out.print(arr.length);
+            break;
+        }
+    }
+}
