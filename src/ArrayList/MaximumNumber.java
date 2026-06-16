bb01ffae1cacf8d6a5cecfffe95c9e41e5c87519
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-16 17:56:51 +0530

    Initial commit

---
+++ src/ArrayList/MaximumNumber.java
@@ -0,0 +1,39 @@
+package ArrayList;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class MaximumNumber {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Size:- ");
+        int size = scan.nextInt();
+
+        if(size <= 0){
+            System.out.println("Array is empty");
+            return;
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        System.out.println("Enter your Array elements- ");
+        for(int i = 0; i<size; i++){
+            list.add(scan.nextInt());
+        }
+
+        System.out.print("Array elements:- ");
+        for(int i = 0; i<size; i++){
+            System.out.print(list.get(i));
+        }
+
+        //------Maximum number;
+        int max = 0;
+        for(int i = 0; i<size; i++){
+            if(list.get(i) > max){
+                max = list.get(i);
+            }
+        }
+
+        System.out.println("\nMaximum number is:- " +max);
+    }
+}
