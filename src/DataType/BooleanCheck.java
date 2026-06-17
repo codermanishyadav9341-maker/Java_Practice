c1dc2c643d12e32b90a61fa64e9c4ef96a77ae7d
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-17 11:48:05 +0530

    Update

---
+++ src/DataType/BooleanCheck.java
@@ -0,0 +1,14 @@
+package DataType;
+
+import java.util.Scanner;
+public class BooleanCheck {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Characters:- ");
+        boolean ch = scan.nextBoolean();
+
+        System.out.println("Check:- " +ch);
+
+    }
+}
