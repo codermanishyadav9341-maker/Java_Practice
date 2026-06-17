98ae9208f699a0d16c174c6d7675714bf9125a41
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-17 17:47:03 +0530

    Update

---
+++ src/DataTypeFloat/Product.java
@@ -0,0 +1,16 @@
+package DataTypeFloat;
+
+import java.util.Scanner;
+public class Product {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your first number:- ");
+        float num1 = scan.nextFloat();
+
+        System.out.print("Enter your Second number:- ");
+        float num2 = scan.nextFloat();
+
+        System.out.println("Product:- " +(num1*num2));
+    }
+}
