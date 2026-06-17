eecfa7b6b56731be8efa4fd2c93cb453f1aa21a8
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-17 18:03:05 +0530

    Update

--- src/DataTypeFloat/Circumference.java
+++ src/DataTypeFloat/Circumference.java
@@ -1,14 +1,14 @@
 package DataTypeFloat;
 
 import java.util.Scanner;
-public class Circle {
+public class Circumference {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter Radius:- ");
         float radius = scan.nextFloat();
 
-        System.out.println("Area of Circle:- " +Math.PI*radius*radius);
+        System.out.println("Circumference of Circle:- " +(2*Math.PI*radius));
 
     }
 }
