74e76c52f0a1a27ba585f1d368b87fb16b3f12a7
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-17 14:13:33 +0530

    Update

---
+++ src/DataType/SmallestNumber.java
@@ -0,0 +1,24 @@
+package DataType;
+
+import java.util.Scanner;
+public class SmallestNumber {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your first number:- ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter your Second number:- ");
+        int num2 = scan.nextInt();
+
+        if(num1 < num2){
+            System.out.println(num1+ " is Smallest number");
+        }
+         else if(num2 < num1){
+             System.out.println(num2+ " is Smallest number");
+        }
+          else{
+              System.out.println("Both are equal");
+        }
+    }
+}
