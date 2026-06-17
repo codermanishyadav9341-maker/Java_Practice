429ff69b4afd02d618f6aecb12b8b314bb26f708
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-17 17:50:31 +0530

    Update

---
+++ src/DataTypeFloat/Divide.java
@@ -0,0 +1,17 @@
+package DataTypeFloat;
+
+import java.util.Scanner;
+public class Divide {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your first number:- ");
+        float num1 = scan.nextFloat();
+
+        System.out.print("Enter your Second number:- ");
+        float num2 = scan.nextFloat();
+
+        System.out.println("Divide:- " +(num1/num2));
+
+    }
+}
