770e1d608506b1828893b8cace9939fc00c6564f
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-17 12:25:33 +0530

    Update

--- src/DataType/ConvertDouble.java
+++ src/DataType/ConvertDouble.java
@@ -1,16 +1,16 @@
 package DataType;
 
 import java.util.Scanner;
-public class ConvertInt {
+public class ConvertDouble {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter your number:- ");
-        int num = scan.nextInt();
+        double num = scan.nextDouble();
 
-        // Int convert into Double;
+        //Double Convert into Int;
+        int result = (int)num;
 
-        double result = num;
-        System.out.println("Double Value:- " +result);
+        System.out.println("Integer Value:- " +result);
     }
 }
