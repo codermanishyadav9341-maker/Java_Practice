8029c706e2fe49c73a9af08a009a19d66c98c956
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-17 18:07:39 +0530

    Update

--- src/DataTypeFloat/Rectangle.java
+++ src/DataTypeFloat/Rectangle.java
@@ -1,4 +1,16 @@
 package DataTypeFloat;
 
+import java.util.Scanner;
 public class Rectangle {
+    public static  void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Length:- ");
+        float length = scan.nextFloat();
+
+        System.out.print("Enter Breadth:- ");
+        float breadth = scan.nextFloat();
+
+        System.out.println("Area of Rectangle:- " +(length*breadth));
+    }
 }
