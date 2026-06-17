c5d9eb663f0105939975dcffb6aa86b366b15659
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-17 15:00:17 +0530

    Update

---
+++ src/DataType/SumDigits.java
@@ -0,0 +1,20 @@
+package DataType;
+
+import java.util.Scanner;
+public class SumDigits {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your number:- ");
+        int num = scan.nextInt();
+
+        int sum = 0;
+        while(num > 0){
+            int digit = num%10;
+              sum = sum + digit;
+              num = num/10;
+        }
+
+        System.out.println("Sum of Digits:- " +sum);
+    }
+}
