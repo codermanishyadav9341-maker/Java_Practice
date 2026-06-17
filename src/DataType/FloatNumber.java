a193faeda55b070408ef74cf5d097083b20258df
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-17 11:38:52 +0530

    Update

--- src/DataType/FloatNumber.java
+++ src/DataType/FloatNumber.java
@@ -1,4 +1,15 @@
 package DataType;
 
+import java.util.Scanner;
 public class FloatNumber {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your number:- ");
+        float num = scan.nextInt();
+
+        System.out.println("Float value of num is:- " +num);
+
+        scan.close();
+    }
 }
