888b418674a9f8c90e239ae42341bbfd1166c2e6
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-17 11:56:10 +0530

    Update

---
+++ src/DataType/Subtract.java
@@ -0,0 +1,18 @@
+package DataType;
+
+import java.util.Scanner;
+public class Subtract {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your first number:- ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter your Second number:- ");
+        int num2 = scan.nextInt();
+
+        System.out.println("Subtract of two number is:- " +(num1-num2));
+
+        scan.close();
+    }
+}
