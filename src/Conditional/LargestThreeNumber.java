6c7033f14337c48cbec85d3c81525c31a412b095
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-18 12:12:52 +0530

    Update

---
+++ src/Conditional/LargestThreeNumber.java
@@ -0,0 +1,27 @@
+package Conditional;
+
+import java.util.Scanner;
+public class LargestThreeNumber {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your first number:- ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter your Second number:- ");
+        int num2 = scan.nextInt();
+
+        System.out.print("Enter your Thirds number:- ");
+        int num3 = scan.nextInt();
+
+        if((num1 >= num2) && (num1 >= num3)){
+            System.out.println(num1+ " is Largest number");
+        }
+         else if((num2 >= num1) && (num2 >= num3)){
+             System.out.println(num2+ " is Largest number");
+        }
+          else{
+              System.out.println(num3+ " is Largest number");
+        }
+    }
+}
