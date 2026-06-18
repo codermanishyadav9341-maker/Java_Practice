286fd001ff7420631dc8b678b45b0b16a7b1b94c
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-06-18 12:07:16 +0530

    Update

---
+++ src/Conditional/LargestNumber.java
@@ -0,0 +1,24 @@
+package Conditional;
+
+import java.util.Scanner;
+public class LargestNumber {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your first number:- ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter your Second number:- ");
+        int num2 = scan.nextInt();
+
+        if(num1 > num2){
+            System.out.println(num1+ " is Largest number");
+        }
+         else if(num2 > num1){
+             System.out.println(num2+ " is Largest number");
+        }
+          else{
+              System.out.println("Both are equal");
+        }
+    }
+}
