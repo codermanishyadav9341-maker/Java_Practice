bdde545c6fca05f6d0b68b887760eb850a8d9237
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-08 17:58:30 +0530

    Initial commit

---
+++ src/Polymorphism_PracticeSet/Fruit.java
@@ -0,0 +1,37 @@
+package Polymorphism_PracticeSet;
+
+public class Fruit {
+    public void color(){
+        System.out.println("Fruit has different color");
+    }
+}
+class Apple extends Fruit{
+    @Override
+    public void color(){
+        System.out.println("Apple color is Red");
+    }
+}
+class Banana extends Fruit{
+    @Override
+    public void color(){
+        System.out.println("Banana color is yellow");
+    }
+}
+class Orange extends Fruit{
+    @Override
+    public void color(){
+        System.out.println("Orange color is orange");
+    }
+
+    public static void main(String[] args){
+        Fruit[] fruit = {
+                new Apple(),
+                new Banana(),
+                new Orange()
+        };
+
+        for(Fruit fruit1: fruit){
+            fruit1.color();
+        }
+    }
+}
